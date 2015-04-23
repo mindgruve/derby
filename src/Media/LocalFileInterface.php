e6a07c34db0079348d1c2f7a5da8f1d8f42b1baf
shideon <shideon@gmail.com>
2015-04-23 19:05:22 +0000

    Updated LocalFileInterface docblocks
    git-svn-id: svn://drobo.mindgruve.com/mindgruve/Libraries/DerbyMediaManager/trunk@65547 b4230220-3ebc-4287-bc35-dec2f0842e9e

--- src/Media/LocalFileInterface.php
+++ src/Media/LocalFileInterface.php
@@ -20,39 +20,46 @@ interface LocalFileInterface extends MediaInterface
     const TYPE_MEDIA_FILE = 'MEDIA\LOCAL_FILE';
 
     /**
+     * Delete file
      * @return bool
      */
     public function delete();
 
     /**
+     * Rename/move file
      * @param $newKey
      * @return mixed
      */
     public function rename($newKey);
 
     /**
+     * Write data to file
      * @param $data
      * @return mixed
      */
     public function write($data);
 
     /**
+     * Read data from file
      * @return boolean
      */
     public function read();
 
     /**
+     * Upload file
      * @param AdapterInterface $adapter
      * @return RemoteFileInterface
      */
     public function upload(AdapterInterface $adapter);
 
     /**
+     * Get file extension
      * @return string
      */
     public function getFileExtension();
 
     /**
+     * Get file mime/media type
      * @return string
      */
     public function getMimeType();
