267a25caee80ff1d4b08d8bcfac6cc088928cafc
shideon <shideon@gmail.com>
2015-04-24 00:14:58 +0000

    Added generic text type (mainly for type hinting)
    git-svn-id: svn://drobo.mindgruve.com/mindgruve/Libraries/DerbyMediaManager/trunk@65578 b4230220-3ebc-4287-bc35-dec2f0842e9e

---
+++ src/Media/Local/Text.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package mindgruve/derby
+ * @copyright (c) 2015 Mindgruve
+ * @author Kevin Simpson <ksimpson@mindgruve.com>
+ * @author John Pancoast <jpancoast@mindgruve.com>
+ */
+
+namespace Derby\Media\Local;
+
+use Derby\Media\LocalFile;
+
+/**
+ * Derby\Media\Local\Text
+ *
+ * @author Kevin Simpson <ksimpson@mindgruve.com>
+ * @author John Pancoast <jpancoast@mindgruve.com>
+ */
+class Text extends LocalFile
+{
+}
