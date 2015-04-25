c9d481abae0f7267fd7561a05c9985678846a9ba
shideon <shideon@gmail.com>
2015-04-25 01:06:40 +0000

    Corrected tpm_path config var location
    git-svn-id: svn://drobo.mindgruve.com/mindgruve/Libraries/DerbyMediaManager/trunk@65658 b4230220-3ebc-4287-bc35-dec2f0842e9e

--- src/Config.php
+++ src/Config.php
@@ -62,11 +62,11 @@ class Config
     public function isConfigValid()
     {
         // although our default config has more settings, the only hard dependency
-        // is [derby][media] and [derby][tmp_path] at the moment. Anything else isn't
+        // is [derby][media] and [derby][defaults][tmp_path] at the moment. Anything else isn't
         // used and not required yet.
         // @todo I think we should look into symfony's tree builder or options resolver
 
-        if (!isset($this->config['derby']['tmp_path'])) {
+        if (!isset($this->config['derby']['defaults']['tmp_path'])) {
             return false;
         }
 
