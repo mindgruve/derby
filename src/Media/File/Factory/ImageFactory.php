a101420276dea4969b0ebb506609772e7388d9d7
Kevin <simpkevin@gmail.com>
2015-10-23 10:52:52 -0700

    adding event dispatcher to image factory

--- src/Media/File/Factory/ImageFactory.php
+++ src/Media/File/Factory/ImageFactory.php
@@ -11,6 +11,7 @@ namespace Derby\Media\File\Factory;
 use Derby\Adapter\FileAdapterInterface;
 use Derby\Media\File\Image;
 use Imagine\Image\AbstractImagine;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Derby\Media\LocalFileFactory\ImageFactory
@@ -20,24 +21,32 @@ use Imagine\Image\AbstractImagine;
  */
 class ImageFactory extends FileFactory
 {
-
+    /**
+     * @var AbstractImagine
+     */
     protected $imagine;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $dispatcher;
 
     /**
      * {@inheritDoc}
      */
     public function build($key, FileAdapterInterface $adapter)
     {
-        return new Image($key, $adapter, $this->imagine);
+        return new Image($key, $adapter, $this->imagine, $this->dispatcher);
     }
 
     /**
      * @param array $extensions
      * @param AbstractImagine $imagine
      */
-    public function __construct(array $extensions,  AbstractImagine $imagine)
+    public function __construct(array $extensions,  AbstractImagine $imagine, EventDispatcherInterface $dispatcher = null)
     {
         $this->imagine = $imagine;
+        $this->dispatcher = $dispatcher;
         $this->setExtensions($extensions);
     }
 }
