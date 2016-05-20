05c6a8614aedac6724fd9d760d17c803ec007975
Kevin <simpkevin@gmail.com>
2016-05-20 12:49:09 -0700

    adding media type for youtube videos

--- src/Media/Embed/YouTube/YouTubeVideo.php
+++ src/Media/Embed/YouTube/YouTubeVideo.php
@@ -7,4 +7,12 @@ use Derby\Media\Embed;
 class YouTubeVideo extends Embed
 {
 
+    const TYPE_MEDIA_EMBED_YOUTUBE = 'MEDIA/EMBED/YOUTUBE';
+
+    public function getMediaType()
+    {
+        return self::TYPE_MEDIA_EMBED_YOUTUBE;
+    }
+
+
 }
