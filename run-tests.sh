802c723da1bb54af26ffcdbb4a10238afaa49170
Kevin <simpkevin@gmail.com>
2016-08-17 23:34:38 -0700

    moving tests directory

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-php bin/phpunit --bootstrap vendor/autoload.php --colors  --stop-on-error src/Tests/Unit/
+php bin/phpunit --bootstrap vendor/autoload.php --colors --stop-on-error tests
 
 
