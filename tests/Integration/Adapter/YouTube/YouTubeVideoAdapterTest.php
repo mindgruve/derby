c12cd23481a819ff9048f964c7b8375b7583da29
Kevin <simpkevin@gmail.com>
2016-08-17 23:55:36 -0700

    skipping youtube integrations if credentials do not exist

--- tests/Integration/Adapter/YouTube/YouTubeVideoAdapterTest.php
+++ tests/Integration/Adapter/YouTube/YouTubeVideoAdapterTest.php
@@ -25,8 +25,7 @@ class YouTubeVideoAdapterTest extends \PHPUnit_Framework_TestCase
     public function setup()
     {
         if (!file_exists(__DIR__.'/../../../credentials.json')) {
-            echo 'YouTube Credentials Do Not Exist'.PHP_EOL;
-            return;
+            $this->markTestSkipped('YouTube Credentials Do Not Exist');
         }
         $this->client = new \Google_Client();
         $credentials = json_decode(file_get_contents(__DIR__.'/../../../credentials.json'), true);
--- tests/Integration/Media/YouTube/YouTubeChannelTest.php
+++ tests/Integration/Media/YouTube/YouTubeChannelTest.php
@@ -53,8 +53,7 @@ class YouTubeChannelTest extends \PHPUnit_Framework_TestCase
     public function setup()
     {
         if (!file_exists(__DIR__.'/../../../credentials.json')) {
-            echo 'YouTube Credentials Do Not Exist'.PHP_EOL;
-            return;
+            $this->markTestSkipped('YouTube Credentials Do Not Exist');
         }
         $this->client = new \Google_Client();
         $credentials = json_decode(file_get_contents(__DIR__.'/../../../credentials.json'), true);
